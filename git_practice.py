aadf5d04a9e8660f81bb83d76036f8bcf1faf577
haninfuaad <mhdhaninfuaad@gmail.com>
2026-09-19 11:48:33 +0530

    initial commit

---
+++ git_practice.py
@@ -0,0 +1,16 @@
+#abc abstract base class,ABC abstractmethod
+from abc import ABC,abstractmethod
+class Polygon(ABC):
+    @abstractmethod
+    def sides(self):
+        print("more than 2 sides")
+class Triangle(Polygon):
+    def sides(self):
+        print("3")
+    def info(self):
+        print("triangle info")
+class Rect(Polygon):
+    def sides(self):
+        print("4")
+t1=Triangle()
+t1.sides()
